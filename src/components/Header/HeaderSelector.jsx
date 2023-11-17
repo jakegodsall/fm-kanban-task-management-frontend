6690a71e83b6ab483f666aeb708c6bb0cbbf71b9
jakegodsall <jake.edward.godsall@gmail.com>
2023-11-17 20:32:23 +0100

    added animation to chevron

--- src/components/Header/HeaderSelector.jsx
+++ src/components/Header/HeaderSelector.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./HeaderSelector.module.css";
 
 import Chevron from "../../assets/icon-chevron-down.svg";
 
 const HeaderSelector = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOnClick = () => {
+        setIsOpen((prevState) => !prevState);
+    };
+
     return (
-        <div className={styles.headerSelector}>
+        <div className={styles.headerSelector} onClick={handleOnClick}>
             <p className={styles.headerSelector__title}>Platform Launch</p>
             <img
-                className={styles.headerSelector__chevron}
+                className={
+                    isOpen
+                        ? `${styles.headerSelector__chevron} ${styles.headerSelector__chevron_open}`
+                        : `${styles.headerSelector__chevron}`
+                }
                 src={Chevron}
                 alt="open selector button"
             />
