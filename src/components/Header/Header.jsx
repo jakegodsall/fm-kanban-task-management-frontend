e2e9556c2108d06e051a23061b07f7a5cb0c0c9d
jakegodsall <jake.edward.godsall@gmail.com>
2023-11-26 17:47:36 +0000

    added functionality to show board selection modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Header.module.css";
 
@@ -7,19 +7,36 @@ import VerticalEllipsis from "../../assets/icon-vertical-ellipsis.svg";
 
 import HeaderSelector from "./HeaderSelector";
 import AddTaskButton from "./AddTaskButton";
+import BoardSelectDropdrown from "../BoardSelectDropdown/BoardSelectDropdown";
 
 const Header = () => {
+    const [boardSelectActive, setBoardSelectActive] = useState(false);
+
+    const handleOpenBoardSelect = () => {
+        setBoardSelectActive((prevState) => !prevState);
+    };
+
     return (
-        <header className={styles.header}>
-            <div className={styles.header__left}>
-                <img className={styles.header__logo} src={Logo} alt="logo" />
-                <HeaderSelector />
-            </div>
-            <div className={styles.header__right}>
-                <AddTaskButton />
-                <img src={VerticalEllipsis} alt="ellipsis" />
-            </div>
-        </header>
+        <>
+            <header className={styles.header}>
+                <div className={styles.header__left}>
+                    <img
+                        className={styles.header__logo}
+                        src={Logo}
+                        alt="logo"
+                    />
+                    <HeaderSelector
+                        boardSelectActive={boardSelectActive}
+                        setBoardSelectActive={handleOpenBoardSelect}
+                    />
+                </div>
+                <div className={styles.header__right}>
+                    <AddTaskButton />
+                    <img src={VerticalEllipsis} alt="ellipsis" />
+                </div>
+            </header>
+            {boardSelectActive && <BoardSelectDropdrown />}
+        </>
     );
 };
 
--- src/components/Header/HeaderSelector.jsx
+++ src/components/Header/HeaderSelector.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 import styles from "./HeaderSelector.module.css";
 
 import Chevron from "../../assets/icon-chevron-down.svg";
 
-const HeaderSelector = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
+const HeaderSelector = ({ boardSelectActive, setBoardSelectActive }) => {
     const handleOnClick = () => {
-        setIsOpen((prevState) => !prevState);
+        setBoardSelectActive();
     };
 
     return (
@@ -16,7 +14,7 @@ const HeaderSelector = () => {
             <p className={styles.headerSelector__title}>Platform Launch</p>
             <img
                 className={
-                    isOpen
+                    boardSelectActive
                         ? `${styles.headerSelector__chevron} ${styles.headerSelector__chevron_open}`
                         : `${styles.headerSelector__chevron}`
                 }
