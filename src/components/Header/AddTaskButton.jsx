254d9eed646c2c06815cf9e9be1d3d8bccdc86ed
jakegodsall <jake.edward.godsall@gmail.com>
2023-12-01 20:03:15 +0000

    added skeleton for AddTaskModal

--- src/components/Header/AddTaskButton.jsx
+++ src/components/Header/AddTaskButton.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import AddTaskMobile from "../../assets/icon-add-task-mobile.svg";
 
 import styles from "./AddTaskButton.module.css";
+import AddTaskModal from "../Modals/AddTaskModal/AddTaskModal";
 
 const AddTaskButton = () => {
     const [addTaskModalOpen, setAddTaskModalOpen] = useState(true);
@@ -12,12 +13,15 @@ const AddTaskButton = () => {
     };
 
     return (
-        <img
-            className={styles.addTaskButton}
-            src={AddTaskMobile}
-            alt="Add Task Button"
-            onClick={handleOpenTaskModal}
-        />
+        <>
+            <img
+                className={styles.addTaskButton}
+                src={AddTaskMobile}
+                alt="Add Task Button"
+                onClick={handleOpenTaskModal}
+            />
+            {addTaskModalOpen && <AddTaskModal />}
+        </>
     );
 };
 
---
+++ src/components/Modals/AddTaskModal/AddTaskModal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import styles from "./AddTaskModal.module.css";
+import Modal from "../../UI/Modal/Modal";
+
+const AddTaskModal = () => {
+    return <Modal></Modal>;
+};
+
+export default AddTaskModal;
